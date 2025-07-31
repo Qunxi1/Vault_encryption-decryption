afd055e873b5f68ce97a39132ac8f265fd1bf141
Qunxi1 <1430305025@qq.com>
2025-07-31 14:27:20 +0800

    change start logic

--- data_function_provider/main.py
+++ data_function_provider/main.py
@@ -9,6 +9,3 @@ app = FastAPI()
 # 挂载两个模块，分别加上前缀
 app.include_router(approval_router, prefix="/approval", tags=["Approval Service"])
 app.include_router(vault_router, prefix="/vault", tags=["Vault Service"])
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9001)
